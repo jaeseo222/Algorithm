adb41f93ad7f0eec9492a1bf7d81a0b01fb8e9c4
woJSwo <68185825+jaeseo222@users.noreply.github.com>
2022-01-22 23:59:33 +0900

    Create 1026.cpp

---
+++ Baekjoon/정렬/1026.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+
+	vector<int> a(n), b(n);
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	for (int i = 0; i < n; i++)
+		cin >> b[i];
+
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end(), greater<>());
+
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+		sum += a[i] * b[i];
+
+	cout << sum;
+}
