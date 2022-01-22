036576753861fcc227f926864c1d3fc2c275243f
woJSwo <68185825+jaeseo222@users.noreply.github.com>
2022-01-22 22:06:12 +0900

    Update 2750.cpp

--- Baekjoon/정렬/2750.cpp
+++ Baekjoon/정렬/2750.cpp
@@ -1,16 +1,29 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
 using namespace std;
+const int SIZE = 1000;
+int arr[SIZE];
+
+void sorted(int arr[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		int idx = i;
+		for (int j = i + 1; j < n; j++) {
+			if (arr[idx] > arr[j])
+				idx = j;
+		}
+		int tmp = arr[i];
+		arr[i] = arr[idx];
+		arr[idx] = tmp;
+	}
+}
+
 int main() {
 	int n;
 	cin >> n;
 
-	vector<int> arr(n);
 	for (int i = 0; i < n; i++)
 		cin >> arr[i];
 
-	sort(arr.begin(), arr.end());
+	sorted(arr, n);
 
 	for (int i = 0; i < n; i++)
 		cout<< arr[i]<<'\n';
