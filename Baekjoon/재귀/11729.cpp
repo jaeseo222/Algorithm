d334edebad57953c1e0f9a68ddba406bc443d633
woJSwo <68185825+jaeseo222@users.noreply.github.com>
2022-02-03 23:41:40 +0900

    Create 11729.cpp

---
+++ Baekjoon/재귀/11729.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath> // pow
+using namespace std;
+void hanoi(int n, int start, int pass, int to) {
+	if (n == 1)
+		cout << start << ' ' << to << '\n';
+	else {
+		hanoi(n - 1, start, to, pass);
+		cout << start << ' ' << to << '\n';
+		hanoi(n - 1, pass, start, to);
+	}
+}
+int main() {
+	int n;
+	cin >> n;
+
+	cout << (int)pow(2, n) - 1 << '\n';
+	hanoi(n, 1, 2, 3);
+}
