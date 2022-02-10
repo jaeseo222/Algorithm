708996c8c31f8d08700327d42b46c83bb67fd3b0
woJSwo <68185825+jaeseo222@users.noreply.github.com>
2022-02-11 01:42:15 +0900

    Update 10814.cpp

--- Baekjoon/정렬/10814.cpp
+++ Baekjoon/정렬/10814.cpp
@@ -1,26 +1,19 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-#include<tuple>
 using namespace std;
-typedef tuple<int, string, int> ti;
-bool cmp(ti& s1, ti& s2) {
-	if (get<0>(s1) != get<0>(s2))
-		return get<0>(s1) < get<0>(s2);
-	return get<2>(s1) < get<2>(s2);
+typedef pair<int, string> ci;
+bool cmp(ci s1, ci s2) {
+	return s1.first < s2.first;
 }
 int main() {
-	int n, a;
-	string b;
+	int n;
 	cin >> n;
 
-	vector<ti> arr(n, ti());
-	for (int i = 0; i < n; i++) {
-		cin >> a >> b;
-		arr[i] = make_tuple(a, b, i);
-	}
-	sort(arr.begin(), arr.end(), cmp);
-
+	vector<ci> arr(n, ci());
 	for (int i = 0; i < n; i++)
-		cout << get<0>(arr[i]) <<' '<< get<1>(arr[i]) << '\n';
+		cin >> arr[i].first >> arr[i].second;
+	stable_sort(arr.begin(), arr.end(), cmp);
+	for (int i = 0; i < n; i++)
+		cout << arr[i].first << ' ' << arr[i].second << '\n';
 }
