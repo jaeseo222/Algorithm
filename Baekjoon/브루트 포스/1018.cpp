abae3f3e7259d03797c5e8b884e130e7e64a3485
woJSwo <68185825+jaeseo222@users.noreply.github.com>
2022-02-27 16:03:14 +0900

    Update 1018.cpp

--- Baekjoon/브루트 포스/1018.cpp
+++ Baekjoon/브루트 포스/1018.cpp
@@ -1,40 +1,30 @@
 #include<iostream>
 #include<vector>
+
 using namespace std;
-const int SIZE = 64;
-
-int solu(vector<vector<char>> &b, int row, int col) {
-	int b_cnt=0;//맨 처음 B 기준으로, 수정해야 되는 개수
-
-	for(int i=0;i<8;i++)
-		for (int j = 0; j < 8; j++) {
-			if ((i + j) % 2 == 0 && b[row + i][col + j] != 'B')
-				b_cnt++;
-			else if ((i + j) % 2 && b[row + i][col + j] != 'W')
-				b_cnt++;
-		}
-
+int solu(int r, int c, vector<vector<char>>& arr) {
+	// 처음이 B로 시작할 경우로 생각
+	int ans = 0;
+	for (int i = r; i < r + 8; i++) {
+		for (int j = c; j < c + 8; j++)
+			if (((i + j) % 2 == 0 && arr[i][j] != 'B') || ((i + j) % 2 && arr[i][j] != 'W'))
+				ans++;
+	}
 	//B, W 중 뭘로 시작해야 덜 수정할 지
-	if (b_cnt > SIZE / 2)
-		return SIZE - b_cnt;
-	return b_cnt;
+	return min(ans, 64 - ans);
 }
-
 int main() {
-	int n, m, ans=SIZE+1;
+	int n, m;
 	cin >> n >> m;
-
-	vector<vector<char>> board(n,vector<char>(m));
+	int ans = n * m;
+	vector<vector<char>> arr(n, vector<char>(m, 0));
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++)
-			cin >> board[i][j];
+			cin >> arr[i][j];
 
-	//범위 주의
-	for (int i = 0; i <= n-8; i++)
-		for (int j = 0; j <= m-8; j++) {
-			int cnt = solu(board, i, j);
-			if (ans > cnt) ans = cnt;
+	for (int i = 0; i <= n - 8; i++)
+		for (int j = 0; j <= m - 8; j++) {
+			ans = min(ans, solu(i, j, arr));
 		}
-
 	cout << ans;
 }
