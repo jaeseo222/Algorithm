097b66b8e08b72df46f9b3c730d540a5e71e56a6
woJSwo <68185825+jaeseo222@users.noreply.github.com>
2022-03-06 23:35:36 +0900

    Update 1946.cpp

--- Baekjoon/정렬/1946.cpp
+++ Baekjoon/정렬/1946.cpp
@@ -1,34 +1,33 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<string>
 using namespace std;
-typedef pair<int, int> pi;
+typedef pair<int, int> ci;
 
-bool cmp(const pi& i1, const pi& i2) { // 없어도 됨.
-	return i1.first < i2.first;
-}
+int solu(vector<ci>& arr, int n) {
+	int ans = 1, rank = arr[0].second; // 서류 1등은 합격, 
 
-int ans(vector<pi> arr) {
-	int sum=1, tmp = arr[0].second; // 최솟값으로 갱신해가면서 비교
-	for (int i = 1; i < arr.size(); i++)
-		if (tmp > arr[i].second) {
-			tmp = arr[i].second;
-			sum++;
-		}
-	return sum;
+	for (int i = 1; i < n; i++) {
+		if (rank > arr[i].second)
+			ans++;
+		rank = min(rank, arr[i].second); // 등수를 최솟값으로 갱신해가면서 비교
+	}
+	return ans;
 }
 
 int main() {
 	int t, n;
 	cin >> t;
 
-	while(t--) {
+	while (t--) {
 		cin >> n;
-		vector<pi> arr(n);
+		vector<ci> arr(n);
 		for (int i = 0; i < n; i++)
 			cin >> arr[i].first >> arr[i].second;
 
-		sort(arr.begin(), arr.end(),cmp);
-		cout << ans(arr) << '\n';
+		sort(arr.begin(), arr.end()); // 우선 내림차순으로 서류면접 순위 결정
+
+		cout << solu(arr, n) << '\n';
 	}
 }
