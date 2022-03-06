eb653afecb07492ebcc19aefe6beec6985532e9b
woJSwo <68185825+jaeseo222@users.noreply.github.com>
2022-03-07 00:42:41 +0900

    Update 12840.cpp

--- Baekjoon/수학/12840.cpp
+++ Baekjoon/수학/12840.cpp
@@ -2,34 +2,54 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
-const int SIZE = 86400; // 하루 총 시간
-void timeInput(int& h, int& m, int& s, int sum) {
-	h = sum / 3600;
-	m = sum / 60 % 60;
-	s = sum % 60;
+const int MINUTE = 60; //1분 초로 나타냄
+const int HOUR = 60 * MINUTE; //1시간 초로 나타냄
+const int DAY = 24 * HOUR; //하루 초로 나타냄
+
+void printTime(int second) {
+	int h = second / HOUR;
+	int m = (second % HOUR) / MINUTE;
+	int s = (second % HOUR) % MINUTE;
+	cout << h << ' ' << m << ' ' << s << '\n';
 }
 
+/**
+ * 단순 구현 문제
+ * 시계를 앞으로 돌리면 +, 뒤로 돌리면 -
+ * !주의! 전날로 넘어갈 수 있으므로 항상 모듈러 연산을 하고,
+ *       그럼에도 전날로 넘어갔을 경우 하루를 더해주는게 중요
+ */
+
 int main() {
+	//입출력 속도 향상
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	int t, h, m, s, c, test;
-	cin >> h >> m >> s >> test;//현재 정보
-	while (test--) {
+
+	int t, h, m, s, c, q;
+	//입력
+	cin >> h >> m >> s;
+	cin >> q;
+
+	//시간 초기화
+	int second = h * HOUR + m * MINUTE + s;
+
+	//입력 & 연산 & 출력
+	while (q--) {
 		cin >> t;
-		int sum = 3600 * h + 60 * m + s; //총 초
-		if (t == 1) {
+
+		if (t == 1) { //시계 앞으로(+)
 			cin >> c;
-			sum += c;
+			second += c;
 		}
-		if (t == 2) {
+		if (t == 2) { //시계 뒤로(-)
 			cin >> c;
-			sum -= c;
-			while (sum < 0)
-				sum += SIZE;
+			second -= c;
+			while (second < 0)
+				second += DAY;
 		}
-		sum %= SIZE;
-		timeInput(h, m, s, sum);
-		if (t == 3)
-			cout << h << ' ' << m << ' ' << s << '\n';
+		if (t == 3) //시계가 가리키는 시간 출력
+			printTime(second);
+
+		second %= DAY; //하루가 넘어갔을 수 있으므로 모듈러 연산
 	}
 }
